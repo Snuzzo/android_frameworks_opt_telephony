lt03lte-cafaosp/platform-frameworks-opt-telephony

Fix CDMA data call issue after SIM refresh.
Icc records are reloaded during Sim refresh init and forces DCT
to reload the APNs. However if the data is not registered during
refresh, dummy APNs for CDMA are not created leading to no data.

To fix this, check the selected iccrecords type and if it is of
type RuimRecords then add dummy data profiles even if data RAT
is unknown.

Change-Id: I2051209ac2b987e8544cd9592b31e87e89971336
CRs-Fixed: 688497

src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -66,6 +66,7 @@ import com.android.internal.telephony.cdma.CdmaSubscriptionSourceManager;
 import com.android.internal.telephony.PhoneConstants;
 import com.android.internal.telephony.RILConstants;
 import com.android.internal.telephony.uicc.IccRecords;
+import com.android.internal.telephony.uicc.RuimRecords;
 import com.android.internal.telephony.uicc.UiccController;
 import com.android.internal.telephony.dataconnection.CdmaDataProfileTracker;
 import com.android.internal.util.AsyncChannel;
@@ -2232,13 +2233,8 @@ public class DcTracker extends DcTrackerBase {
             }
         }
 
-        if (mAllDps.isEmpty()) {
-            int radioTech = mPhone.getServiceState().getRilDataRadioTechnology();
-            if (!CdmaDataProfileTracker.OMH_ENABLED &&
-                    UiccController.getFamilyFromRadioTechnology(radioTech)
-                    == UiccController.APP_FAM_3GPP2) {
-                addDummyDataProfiles(operator);
-            }
+        if (mAllDps.isEmpty() && isDummyProfileNeeded()) {
+            addDummyDataProfiles(operator);
         }
 
         if (mAllDps.isEmpty()) {
@@ -2255,6 +2251,19 @@ public class DcTracker extends DcTrackerBase {
             if (DBG) log("createAllApnList: mPreferredApn=" + mPreferredDp);
         }
         if (DBG) log("createAllApnList: X mAllDps=" + mAllDps);
+    }
+
+    private boolean isDummyProfileNeeded() {
+        boolean ret = !CdmaDataProfileTracker.OMH_ENABLED;
+        int radioTech = mPhone.getServiceState().getRilDataRadioTechnology();
+        int radioTechFam = UiccController.getFamilyFromRadioTechnology(radioTech);
+        IccRecords r = mIccRecords.get();
+        if (DBG) log("isDummyProfileNeeded: radioTechFam = " + radioTechFam);
+        // If uicc app family based on data rat is unknown,
+        // check if records selected is RuimRecords.
+        return ret && (radioTechFam == UiccController.APP_FAM_3GPP2 ||
+                ((radioTechFam == UiccController.APP_FAM_UNKNOWN) &&
+                (r != null) && (r instanceof RuimRecords)));
     }
 
     private void addDummyDataProfiles(String operator) {

Snuzzo/android_frameworks_opt_telephony

src/java/com/android/internal/telephony/dataconnection/DcTracker.java
@@ -83,6 +83,7 @@ import com.android.internal.telephony.PhoneConstants;
 import com.android.internal.telephony.RILConstants;
 import com.android.internal.telephony.SubscriptionController;
 import com.android.internal.telephony.uicc.IccRecords;
+import com.android.internal.telephony.uicc.RuimRecords;
 import com.android.internal.telephony.uicc.UiccController;
 import com.android.internal.telephony.uicc.IccUtils;
 import com.android.internal.util.AsyncChannel;
@@ -2727,11 +2728,8 @@ public final class DcTracker extends DcTrackerBase {
 
         dedupeApnSettings();
 
-        if (mAllApnSettings.isEmpty()) {
-            if(UiccController.getFamilyFromRadioTechnology(radioTech)
-                    == UiccController.APP_FAM_3GPP2) {
-                addDummyApnSettings(operator);
-            }
+        if (mAllApnSettings.isEmpty() && isDummyProfileNeeded()) {
+            addDummyApnSettings(operator);
         }
 
         addEmergencyApnSetting();
@@ -2858,6 +2856,18 @@ public final class DcTracker extends DcTrackerBase {
                 roamingProtocol, dest.carrierEnabled, dest.bearer, dest.profileId,
                 (dest.modemCognitive || src.modemCognitive), dest.maxConns, dest.waitTime,
                 dest.maxConnsTime, dest.mtu, dest.mvnoType, dest.mvnoMatchData);
+    }
+
+    private boolean isDummyProfileNeeded() {
+        int radioTech = mPhone.getServiceState().getRilDataRadioTechnology();
+        int radioTechFam = UiccController.getFamilyFromRadioTechnology(radioTech);
+        IccRecords r = mIccRecords.get();
+        if (DBG) log("isDummyProfileNeeded: radioTechFam = " + radioTechFam);
+        // If uicc app family based on data rat is unknown,
+        // check if records selected is RuimRecords.
+        return (radioTechFam == UiccController.APP_FAM_3GPP2 ||
+                ((radioTechFam == UiccController.APP_FAM_UNKNOWN) &&
+                (r != null) && (r instanceof RuimRecords)));
     }
 
     private void addDummyApnSettings(String operator) {